6fcc8296fa4e9a7e3419e262a692736667e109f3
Calvin Ren <j.calvin.ren@gmail.com>
2024-11-08 22:34:37 +1000

    create worker.js in web

---
+++ web/worker/worker.js
@@ -0,0 +1,7 @@
+window.postMessageToFindMistakesWorker = async function (locale, text) {
+    try {
+        console.log('postMessageToFindMistakesWorker', locale, text);
+    } catch (error) {
+        throw error;
+    }
+};
